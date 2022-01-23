ca2aba6dc2b4d8d65daad2cfdf7c795ee3c1f80e
VB PROGER <97066898+VBPROGER@users.noreply.github.com>
2022-01-23 21:42:51 +0300

    Create pypink_tester.py

---
+++ src/pypink_tester.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# Welcome to PyPink oficial testing script.
+import pypink # This function imports PyPink.
+# Now let's get started.
+print(pypink.alphabet())
+# This function prints english alphabet.
+print(pypink.alphabet(lang='ru'))
+# This function prints russian alphabet.
+print(pypink.alphabet(lang='en'))
+# This function prints english alphabet (but with "lang" option).
